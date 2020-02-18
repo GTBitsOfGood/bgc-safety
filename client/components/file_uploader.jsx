2f495c789324462939b7760cd68f37c5b338e17a
Emily Wilson <emilywilson@Emilys-MacBook-Pro-3.local>
2020-02-18 18:03:49 -0500

    change CSV upload to class

--- client/components/file_uploader.jsx
+++ client/components/file_uploader.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const FileUploader = () => (
-  <div className="upload-container">
-    <label htmlFor="file-upload" className="file-upload">
-      <i className="fa fa-plus" />
-      Click to upload CSV
-      <input id="file-upload" type="file" accept="csv" />
-    </label>
-  </div>
-);
+const FileUploader = props => {
+  const { onChange } = props;
+
+  return (
+    <div className="upload-container">
+      <label htmlFor="file-upload" className="file-upload">
+        <i className="fa fa-plus" />
+        Click to upload CSV
+        <input id="file-upload" type="file" accept="csv" onChange={onChange} />
+      </label>
+    </div>
+  );
+};
+
+FileUploader.propTypes = {
+  onChange: PropTypes.func.isRequired
+};
 
 export default FileUploader;
--- pages/csv_upload.jsx
+++ pages/csv_upload.jsx
@@ -1,45 +1,54 @@
 import React from "react";
 
-import { helloWorld } from "../client/actions/api";
+// import { helloWorld } from "../client/actions/api";
 import FileUploader from "../client/components/file_uploader";
 
-const CSVUpload = () => {
-  return (
-    <>
-      <div className="container">
-        <div>
-          <div>
-            <bold className="label">Title:</bold>
-            <input className="text-field" placeholder="Type here" />
-          </div>
-          <div>
-            <bold className="label">Upload:</bold>
-            <FileUploader />
-          </div>
-        </div>
-        <div className="button-container">
-          <button type="button" className="btn btn-danger">
-            Cancel
-          </button>
-          <button type="button" className="btn btn-success">
-            Upload
-          </button>
-        </div>
-      </div>
-    </>
-  );
-};
+class CSVUpload extends React.Component {
+  constructor(props) {
+    super(props);
 
-CSVUpload.getInitialProps = async () => {
-  return helloWorld()
-    .then(payload => {
-      return {
-        message: payload
-      };
-    })
-    .catch(error => ({
-      errorMessage: error.message
-    }));
-};
+    this.state = {
+      uploadedFile: false
+    };
+
+    this.handleUpload = this.handleUpload.bind(this);
+  }
+
+  handleUpload() {
+    this.uploadedFile = true;
+    //
+  }
+
+  render() {
+    const { uploadedFile } = this.state;
+
+    return (
+      <>
+        <div className="container">
+          <div>
+            <div>
+              <bold className="label">Title:</bold>
+              <input className="text-field" placeholder="Type here" />
+            </div>
+            {uploadedFile && (
+              <div>
+                <bold className="label">Upload:</bold>
+                <FileUploader onChange={this.handleUpload} />
+              </div>
+            )}
+          </div>
+          <div className="button-container">
+            <button type="button" className="btn btn-danger">
+              Cancel
+            </button>
+            <button type="button" className="btn btn-success">
+              Upload
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
 
 export default CSVUpload;
