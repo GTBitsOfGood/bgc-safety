7ed1d517c9c4eedce6f23770bc21290195595ed5
Matthew Brown <matthewbrown@gatech.edu>
2020-03-03 19:09:55 -0500

    API to Save Attendance Data (#23)
    * Adds /checkIn endpoint to push entry to student check-in times array.

--- utils/urls.js
+++ utils/urls.js
@@ -19,6 +19,7 @@ export default {
     example: "/api/example",
     student: "/api/student",
     club: "/api/club",
-    school: "/api/school"
+    school: "/api/school",
+    checkIn: "/api/checkIn"
   }
 };
