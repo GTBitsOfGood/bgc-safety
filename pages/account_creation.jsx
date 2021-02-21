d66847f127a29b49379cbd7a82966636daffb338
Otis Smith <themroats@gmail.com>
2021-02-21 16:30:11 -0500

    Add modal, keep form elements in React state

--- pages/account_creation.jsx
+++ pages/account_creation.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Button, TextField, Select } from "@material-ui/core";
+import {
+  Button,
+  TextField,
+  Select,
+  MenuItem,
+  InputLabel,
+  Modal,
+  Backdrop,
+  Fade
+} from "@material-ui/core";
+
 import Link from "next/link";
 import urls from "../utils/urls";
 
 const fetch = require("node-fetch");
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(theme => ({
   container: {
     display: "flex",
     flexDirection: "column",
@@ -16,15 +26,58 @@ const useStyles = makeStyles(() => ({
   userTypes: {
     display: "flex",
     flexDirection: "row",
-    alignItems: "center",
+    alignItems: "center"
   },
   Button: {
     padding: "10px"
+  },
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3)
   }
 }));
 
 const AccountCreation = () => {
   const classes = useStyles();
+  const [type, setType] = React.useState("Administrator");
+  const [open, setOpen] = React.useState(false);
+
+  const [first, setFirst] = React.useState("");
+  const [last, setLast] = React.useState("");
+  const [location, setLocation] = React.useState("");
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleFirstNameChange = e => {
+    setFirst(e.target.value);
+  };
+  const handleLastNameChange = e => {
+    setLast(e.target.value);
+  };
+  const handleLocationChange = e => {
+    setLocation(e.target.value);
+  };
+  const handleEmailChange = e => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange = e => {
+    setPassword(e.target.value);
+  };
 
   return (
     <div className={classes.container}>
@@ -32,19 +85,31 @@ const AccountCreation = () => {
       <p>Select account type for user*</p>
       <div className={classes.userTypes}>
         <Button
-          variant="contained"
+          variant={type !== "Administrator" ? "outlined" : "contained"}
           color="primary"
-          className={classes.userTypeBtnActive}
+          onClick={() => setType("Administrator")}
         >
           Administrator
         </Button>
-        <Button variant="outlined" color="primary">
+        <Button
+          variant={type !== "Bus Driver" ? "outlined" : "contained"}
+          color="primary"
+          onClick={() => setType("Bus Driver")}
+        >
           Bus Driver
         </Button>
-        <Button variant="outlined" color="primary">
+        <Button
+          variant={type !== "Executive Director" ? "outlined" : "contained"}
+          color="primary"
+          onClick={() => setType("Executive Director")}
+        >
           Executive Director
         </Button>
-        <Button variant="outlined" color="primary">
+        <Button
+          variant={type !== "Regional Director" ? "outlined" : "contained"}
+          color="primary"
+          onClick={() => setType("Regional Director")}
+        >
           Regional Director
         </Button>
       </div>
@@ -55,25 +120,81 @@ const AccountCreation = () => {
             id="first-name"
             label="First name"
             defaultValue=" "
+            onChange={handleFirstNameChange}
           />
           <TextField
             required
             id="last-name"
             label="Last name"
             defaultValue=" "
+            onChange={handleLastNameChange}
           />
         </div>
-        <TextField required id="location" label="Location" defaultValue=" " />
+        <TextField
+          required
+          id="location"
+          label="Location"
+          defaultValue=" "
+          onChange={handleLocationChange}
+        />
         <div className={classes.row3}>
           <TextField
             required
             id="email-address"
             label="Email address"
             defaultValue=" "
+            onChange={handleEmailChange}
           />
-          <TextField required id="password" label="Password" defaultValue=" " />
-          <Select />
+          <TextField
+            required
+            id="password"
+            label="Password"
+            defaultValue=" "
+            onChange={handlePasswordChange}
+          />
+          <InputLabel shrink>Assigned Bus Routes</InputLabel>
+          <Select>
+            <MenuItem>Bus Route 1</MenuItem>
+            <MenuItem>Bus Route 2</MenuItem>
+            <MenuItem>Bus Route 3</MenuItem>
+          </Select>
         </div>
+      </div>
+      <div>
+        <Button onClick={handleOpen} variant="contained" color="primary">
+          Create Account
+        </Button>
+
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          className={classes.modal}
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500
+          }}
+        >
+          <Fade in={open}>
+            <div className={classes.paper}>
+              <h2 id="transition-modal-title">Confirm account details</h2>
+
+              <p id="transition-modal-description">
+                Once confirmed, account information will be sent out to the
+                email address.
+              </p>
+              <br />
+              <p>Account type: {type}</p>
+              <p>Name: {first + " " + last}</p>
+              <p>Location: {location}</p>
+              <p>Email Address: {email}</p>
+              <p>Password: {password}</p>
+              <p>Bus Routes: {"BusA, BusB, BusC"}</p>
+            </div>
+          </Fade>
+        </Modal>
       </div>
     </div>
   );
