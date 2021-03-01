f1ba3b59dce64f0ead86b1c5d312ebbf0de9db0a
Jose <josealexiscon@gmail.com>
2021-03-01 18:47:39 -0500

    initial connection with next-auth js and auth0

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { helloWorld } from "../client/actions/api";
+import Link from "next/link";
+import {signIn, signOut, useSession} from "next-auth/client";
+
 
 const HomePage = () => {
   const [payload, setPayload] = React.useState("");
-
+  const [session, loading] = useSession();
   React.useEffect(() => {
     // Example how to create page without ssr
     helloWorld().then(resp => {
@@ -13,6 +16,18 @@ const HomePage = () => {
 
   return (
     <>
+      {!session && (
+        <>
+        Not signed in <br/>
+        <button onClick={signIn}>click to sign in</button>
+        </>
+      )}
+      {session && (
+        <>
+        Signed in as {session.user.email} <br/>
+        <button onClick = {signOut}>click to sign out</button>
+        </>
+      )}
       <h2>Welcome to Next.js!</h2>
       <h3>
         This page is static rendered, because all API calls are made in
