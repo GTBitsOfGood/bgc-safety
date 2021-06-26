b2df4b42f706807e9dd82eb2099839b831ada741
Luke Kim <39681900+luke9kim8@users.noreply.github.com>
2021-06-26 16:15:26 -0400

    Revert "add navbar style"

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { withRouter } from "next/router";
 import Link from "next/link";
-import styled from 'styled-components'
 import PropTypes from "prop-types";
 import {
   AppBar,
@@ -9,16 +8,14 @@ import {
   MenuItem,
   IconButton,
   Menu,
-  Typography,
-  SwipeableDrawer,
-  Drawer
+  Typography
 } from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
 import { makeStyles } from "@material-ui/core/styles";
 
 import routes from "../../utils/routes";
 import Axios from "axios";
-import { lightgray } from "color-name";
+import {getSession, useSession} from "next-auth/client";
 // import { Route } from 'react-router-dom';
 
 const getDate = () => {
@@ -56,34 +53,6 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1
   },
 
-  menu: {
-    width: 250,
-    flexShrink: 0,
-    color: lightgray,
-    // backgroundColor: 'black'
-  },
-
-  menuItems: {
-    width: 250,
-    textDecoration: "none",
-    lineHeight: "25px",
-    fontSize: "20px",
-    color: lightgray
-  },
-
-  menuFont: {
-    fontFamily: "Raleway",
-    textDecoration: "none",
-    fontStyle: "normal",
-    fontWeight: "bold",
-    fontSize: "20px",
-    lineHeight: "25px",
-    color: '#000000 25%',
-    left: 32,
-    top: 40
-  },
-
-
   menuButton: {
     marginRight: theme.spacing(2)
   },
@@ -98,42 +67,47 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-
-
 const Header = props => {
+  console.log("rendering")
   const { defaultSelected, router } = props;
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [selected, setSelected] = React.useState(defaultSelected);
+  const [filteredRoutes, setFilteredRoutes] = React.useState([]);
+  const [session, loading] = useSession()
+
+
+  const filterRoutes = (currentUser) => {
+    let fRoutes = []
+    if(currentUser.type == "Admin"){
+      fRoutes = routes.filter(item => item.type == "Admin" || item.type == "All")
+    } else if (currentUser.type == "BusDriver"){
+      fRoutes = routes.filter(item => item.type == "BusDriver" || item.type == "All")
+    } else if (currentUser.type == "ClubDirector"){
+      fRoutes = routes.filter(item => item.type == "ClubDirectorAttendanceClerk" || item.type == "All")
+    } else if (currentUser.type == "AttendanceClerk") {
+      fRoutes = routes.filter(item => item.type == "ClubDirectorAttendanceClerk"|| item.type == "All")
+    } else {
+      fRoutes = routes.filter(item => item.type == "All")
+    }
+
+    return fRoutes
+
+  }  
+  const queryUser = async () => {
+    console.log("QUERY USER", session.user.email)
+    const res = await fetch(
+      
+      `/api/user?email=${session.user}`
+    );
+    return await res.json();
+  }
+  
+  
+  
+
   const open = Boolean(anchorEl);
-  // const [currentUser, setCurrentUser] = React.useState(null);
-  const currentUser = {
-        BGCMA_email: "sahya",
-        password: '$2a$10$/NYjx/SvECs8YZEYfS4HMOkfZvrYcO5hqERWOyYAEka5vTsgQOZgS',
-        type: "ClubDirector",
-        club: "All"
-      };
-
-  const NavLink = styled.a`
-      text-decoration: none;
-      &:active {
-        color: black;
-        background: lightgray;
-      }
-      &:visited {
-        color:black;
-      }
-      &:hover {
-        color: black;
-        ;
-      }
-    `
-
-  // console.log(currentUser);
-  let filteredRoutes = [];
-  // const [filteredRoutes, setFilteredRoutes] = React.useState([]);
-
-
+  
 
   router &&
     router.events &&
@@ -153,28 +127,17 @@ const Header = props => {
     setAnchorEl(null);
   };
 
-  const filterRoutes = (currentUser) => {
-    // console.log("here")
-    if(currentUser.type == "Admin"){
-      filteredRoutes = routes.filter(item => item.type == "Admin" || item.type == "All")
-    } else if (currentUser.type == "BusDriver"){
-      filteredRoutes = routes.filter(item => item.type == "BusDriver" || item.type == "All")
-    } else if (currentUser.type == "ClubDirector"){
-      console.log("here")
-      filteredRoutes = routes.filter(item => item.type == "ClubDirectorAttendanceClerk" || item.type == "All")
-      // filteredRoutes = setFilteredRoutes(routes.filter(item => item.type == "ClubDirectorAttendanceClerk" || item.type == "All"))
-      // console.log(routes.filter(item => item.type == "ClubDirectorAttendanceClerk" || item.type == "All"))
-      // filteredRoutes = routes.filter(item => item.type == "ClubDirectorAttendanceClerk" && item.type == "All")
-    } else if (currentUser.type == "AttendanceClerk") {
-      filteredRoutes = routes.filter(item => item.type == "ClubDirectorAttendanceClerk"|| item.type == "All")
-    } else {
-      filteredRoutes = routes.filter(item => item.type == "All")
+  React.useEffect(() => async () => {
+    if (!loading && session && filteredRoutes.length == 0) {
+      let currentUser = await queryUser()
+      setFilteredRoutes(filterRoutes(currentUser))
     }
-
-    // console.log(filteredRoutes)
-
-    // setFilteredRoutes(filteredRoutes)
-
+  })
+  
+  if (loading || !session) {
+    // console.log(loading)
+    // console.log(session)
+    return null;
   }
 
   return (
@@ -191,11 +154,8 @@ const Header = props => {
           >
             <MenuIcon />
           </IconButton>
-          <SwipeableDrawer
-            elevation
+          <Menu
             id="menu-appbar"
-            className= "menu"
-            classes={{paper: classes.menuItems}}
             anchorEl={anchorEl}
             anchorOrigin={{
               vertical: "top",
@@ -209,19 +169,15 @@ const Header = props => {
             open={open}
             onClose={handleClose}
           >
-            {filterRoutes(currentUser)}
-            {/* {console.log(filteredRoutes)} */}
-            {filteredRoutes.map((route, index) => (
-              <MenuItem className = {classes.menuFont} onClick={handleClose} key={index}>
-                <Link href={route.link} passHref>
-                  <NavLink>{route.name}</NavLink>
-                </Link>
-                {/* <Link href={route.link}><a style={{textDecoration: "none", color: lightgray}}>{route.name}</a></Link> */}
+            
+            {filteredRoutes ? filteredRoutes.map((route, index) => (
+              <MenuItem onClick={handleClose} key={index}>
+                <Link href={route.link}>{route.name}</Link>
               </MenuItem>
-            ))}
+            )) : <div/>}
 
-            <MenuItem className={classes.menuFont} onClick={handleClose}>My profile</MenuItem>
-          </SwipeableDrawer>
+            <MenuItem onClick={handleClose}>My account</MenuItem>
+          </Menu>
           <Typography variant="h6" className={classes.title}>
             {selected}
           </Typography>
@@ -234,44 +190,14 @@ const Header = props => {
 Header.propTypes = {
   defaultSelected: PropTypes.string.isRequired,
   router: PropTypes.shape({
-    event: PropTypes.shape,
+    event: PropTypes.object,
     pathname: PropTypes.string
-  }).isRequired
+  }).isRequired,
 };
 
-// get endpoint here
-// This function gets called at build time
-// export async function getStaticPaths() {
-
-//   console.log("here")
-
-//   // const currentUser = await Axios.get('/api/user')
-
-//   const currentUser = {
-//     BGCMA_email: "sahya",
-//     password: '$2a$10$/NYjx/SvECs8YZEYfS4HMOkfZvrYcO5hqERWOyYAEka5vTsgQOZgS',
-//     type: "ClubDirector",
-//     club: "All"
-//   }
-
-
-//   if(currentUser.type == "Admin"){
-//     newRoutes = routes.filter(item => item.type == "Admin" && item.type == "All")
-//   } else if (currentUser.type == "BusDriver"){
-//     newRoutes = routes.filter(item => item.type == "BusDriver" && item.type == "All")
-//   } else if (currentUser.type == "ClubDirector"){
-//     console.log("here")
-//     newRoutes = routes.filter(item => item.type == "ClubDirectorAttendanceClerk" && item.type == "All")
-//   } else if (currentUser.type == "AttendanceClerk") {
-//     newRoutes = routes.filter(item => item.type == "ClubDirectorAttendanceClerk" && item.type == "All")
-//   } else {
-//     newRoutes = routes.filter(item => item.type == "All")
-//   }
-
-//   console.log(newRoutes)
-
-//   return {filteredRoutes: newRoutes}
-
-// }
+Header.defaultProps = {
+  defaultSelected: null,
+  router: null,
+};
 
 export default withRouter(Header);
